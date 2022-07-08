d3e4802f921aff35723019bdadf60e8ef4015b5b
Harsh Prasad <harshisindian@gmail.com>
2022-07-09 00:18:03 +0530

    Type of

---
+++ JavaScript/01 Starting with Javascript/03 Type of.js
@@ -0,0 +1,18 @@
+var a = 1 / 0
+console.log(a)
+
+console.log(Infinity / Infinity) // Not A Number
+console.log(Infinity + 1)
+console.log(Infinity - 1)
+console.log(Infinity * Infinity)
+
+console.log(Math.pow(2, 5))
+console.log(Math.pow(2, 5000))
+
+console.log(Number.MAX_VALUE)
+
+console.log(parseInt("100"))
+console.log(parseInt("asdf"))
+
+console.log(typeof(a))
+console.log(typeof(true))
