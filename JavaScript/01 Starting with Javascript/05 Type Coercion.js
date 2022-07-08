8334890fadd65e2245093f0fba0ed23a29ee65a7
Harsh Prasad <harshisindian@gmail.com>
2022-07-09 00:39:30 +0530

    Type Coercion

---
+++ JavaScript/01 Starting with Javascript/05 Type Coercion.js
@@ -0,0 +1,6 @@
+console.log("Harsh" + " Prasad")
+console.log("1" + 2)
+console.log(1 + "Hi") // In Case Of + Concatinations Happens And In Rest It Performs Normal Operations
+console.log(1 - "1")
+console.log(10 * "6")
+console.log(5 / "2")
