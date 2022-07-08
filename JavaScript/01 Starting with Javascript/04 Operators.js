7674404855e348d4d1ccc2735ee99b4be8bd7679
Harsh Prasad <harshisindian@gmail.com>
2022-07-09 00:36:42 +0530

    Operators

---
+++ JavaScript/01 Starting with Javascript/04 Operators.js
@@ -0,0 +1,14 @@
+a = 100
+b = 20
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a == b)
+console.log(a != b)
+console.log(a >> 4)
+console.log(a << 4)
+console.log(a > 50 && b < 50)
+console.log(a > 50 && b > 50)
+console.log(a > 50 || b < 50)
+console.log(a > 50 || b > 50)
