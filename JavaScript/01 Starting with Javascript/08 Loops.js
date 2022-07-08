3a36ce650d5cfd0fdab0b23980bee3a23028a61f
Harsh Prasad <harshisindian@gmail.com>
2022-07-09 01:04:52 +0530

    Loops

---
+++ JavaScript/01 Starting with Javascript/08 Loops.js
@@ -0,0 +1,16 @@
+var  i = 0
+while(i < 5){
+  console.log("While Loop")
+  i += 1
+}
+
+for (i = 0;i < 5; i++){
+  console.log("For Loop")
+}
+
+i = 0
+do{
+  console.log("Do WHile Loop")
+  i += 1
+}
+while(i < 5)
