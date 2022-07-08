4e4ff0e619daaf0fab5b16d1407cc49c6c33b06a
Harsh Prasad <harshisindian@gmail.com>
2022-07-09 00:48:13 +0530

    Conditionals

---
+++ JavaScript/01 Starting with Javascript/07 Conditionals.js
@@ -0,0 +1,56 @@
+var a = -2
+if(a > 0)
+{
+  console.log("Positive")
+}
+else if(a < 0)
+{
+  console.log("Negative")
+}
+else
+{
+  console.log("Zero")
+}
+
+var a = 0
+var b = -3
+var c = "Hello"
+var d = ""
+var e = null
+var f = undefined
+
+if(a){
+  console.log("True")
+}else{
+  console.log("False")
+}
+
+if(b){
+  console.log("True")
+}else{
+  console.log("False")
+}
+
+if(c){
+  console.log("True")
+}else{
+  console.log("False")
+}
+
+if(d){
+  console.log("True")
+}else{
+  console.log("False")
+}
+
+if(e){
+  console.log("True")
+}else{
+  console.log("False")
+}
+
+if(f){
+  console.log("True")
+}else{
+  console.log("False")
+}
