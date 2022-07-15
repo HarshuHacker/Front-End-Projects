6932c917caa83c7a968fb89c6a70cefdd5d6ea06
Harsh Prasad <harshisindian@gmail.com>
2022-07-15 19:58:29 +0530

    Functions on Arrays

---
+++ JavaScript/02 Functions and Arrays/06 Functions on Arrays.js
@@ -0,0 +1,11 @@
+arr = [1, 2, 3, 4, 5, 6, 17]
+console.log(arr)
+arr.push(10)
+console.log(arr)
+arr.pop()
+console.log(arr)
+var a = arr.shift() // This Will Remove The Element From 0th Index And Shift All The Other Elements Towards Left by 1
+console.log(a)
+console.log(arr)
+arr.unshift(10) // This Will Shift All The Other Elements Towards Right by 1 And Insert An Element At 0th Index
+console.log(arr)
