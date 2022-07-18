b1889431849181816c7f6c3ab2631223c12deca2
Harsh Prasad <harshisindian@gmail.com>
2022-07-18 13:23:33 +0530

    Nested Objects

---
+++ JavaScript/03 Objects and Timing Events/05 Nested Objects.js
@@ -0,0 +1,15 @@
+var student = {
+  "name": "Harsh Prasad",
+  age: 21,
+  marks: 92,
+  "200": "Two Hundred",
+  address: {
+    city: "Jaigaon",
+    Pincode: 736182
+  }
+}
+
+console.log(student)
+console.log(student.address)
+console.log(student.address.city)
+console.log(student.address["city"])
