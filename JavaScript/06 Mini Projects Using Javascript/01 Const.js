0db4c87ab149dedf051d25da8ce639c6103e6f72
Harsh Prasad <harshisindian@gmail.com>
2022-07-29 14:22:54 +0530

    Const

---
+++ JavaScript/06 Mini Projects Using Javascript/01 Const.js
@@ -0,0 +1,8 @@
+// console.log(a) "Cannot access 'a' before initialization" we cannot access const before initialization
+const a = 10
+
+// const b We cannot declare const without initialization
+// b = 20
+
+// a = 30 Assignment to constant variable Is Not Allowed
+console.log(a)
