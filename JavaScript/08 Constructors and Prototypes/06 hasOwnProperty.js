0ab85b419c5ad156bb3f9e1ac206d79e5f2a5a1b
Harsh Prasad <harshisindian@gmail.com>
2022-08-04 23:29:04 +0530

    hasOwnProperty

--- JavaScript/08 Constructors and Prototypes/06 hasOwnProperty.js
+++ JavaScript/08 Constructors and Prototypes/06 hasOwnProperty.js
@@ -6,3 +6,10 @@ var p1 = new Person(9);
 console.log(Person.hasOwnProperty("address"));
 console.log(Person.hasOwnProperty("name"));
 console.log(Person.hasOwnProperty("prototype"));
+
+console.log();
+
+console.log(p1.hasOwnProperty("age"));
+console.log(p1.hasOwnProperty("name"));
+console.log(p1.hasOwnProperty("address"));
+console.log(p1.hasOwnProperty("prototype"));
