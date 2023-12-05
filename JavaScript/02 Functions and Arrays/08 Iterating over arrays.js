0ac2a93619d201256949771e475e50d13f249e81
HarshuHacker <75941291+HarshuHacker@users.noreply.github.com>
2023-12-05 09:48:25 +0530

    Update 08 Iterating over arrays.js

--- JavaScript/02 Functions and Arrays/08 Iterating over arrays.js
+++ JavaScript/02 Functions and Arrays/08 Iterating over arrays.js
@@ -12,3 +12,10 @@ function printFunction(element) // Except For element we can pass index and arra
 }
 
 arr.forEach(printFunction) // This Will Call The Function For Each Element Of The Array.
+
+function printFunction(element, index, arr) // Except For element we can pass index and array as 2nd and 3rd argument respectively.
+{
+  console.log(element, index, arr)
+}
+
+arr.forEach(printFunction) // This Will Call The Function And Pass Each Element, index and the complete array itself
